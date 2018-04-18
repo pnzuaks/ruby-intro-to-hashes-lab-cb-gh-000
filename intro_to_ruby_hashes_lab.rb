5dffc73ab5ce2c1aab3eec8aacc5d9ea227626b4
Patricia Nykaza <pnykaza@calstatela.edu>
2018-04-18 19:00:34 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,13 +19,13 @@ def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {
 	    :pieces => 4,
-	    :rent_in_dollars => {},
 	    :names => {
 	      :reading_railroad => {},
   	    :pennsylvania_railroad => {},
   	    :b_and_o_railroad => {},
   	    :shortline => {}
-	    }
+	    },
+	    :rent_in_dollars => {}
 	  }
 	}
 	
